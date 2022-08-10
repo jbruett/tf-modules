c8d27fd61b5b36263b96a4eed9e82125371d1c0d
John Bruett <jbruett@gmail.com>
2022-08-09 21:26:05 -0400

    update private/public subject network acls

--- default-vpc/main.tf
+++ default-vpc/main.tf
@@ -31,11 +31,9 @@ resource "aws_default_route_table" "default" {
 resource "aws_default_network_acl" "default" {
   default_network_acl_id = aws_default_vpc.default.default_network_acl_id
   # module.default_vpc.public_subnet[0]["us-east-1a"].id
-  subnet_ids = [
-    aws_default_subnet.private_subnet_0.id,
-    aws_default_subnet.private_subnet_1.id,
-    aws_default_subnet.public_subnet_0.id,
-    aws_default_subnet.public_subnet_1.id
+  subnet_ids = [concat(
+    aws_default_subnet.private_subnet[*].id,
+    aws_default_subnet.public_subnet[*].id)
   ]
 
   ingress {
