c1ddafb448c253da8f441555cd7c305a58262352
John Bruett <jbruett@gmail.com>
2022-08-09 21:39:38 -0400

    [default-vpc] fix default network acl

--- default-vpc/main.tf
+++ default-vpc/main.tf
@@ -31,10 +31,7 @@ resource "aws_default_route_table" "default" {
 resource "aws_default_network_acl" "default" {
   default_network_acl_id = aws_default_vpc.default.default_network_acl_id
   # module.default_vpc.public_subnet[0]["us-east-1a"].id
-  subnet_ids = [concat(
-    aws_default_subnet.private_subnet[*].id,
-    aws_default_subnet.public_subnet[*].id)
-  ]
+  subnet_ids = concat([for sn in aws_default_subnet.private_subnet : sn.id],[for sn in aws_default_subnet.public_subnet : sn.id])
 
   ingress {
     protocol   = -1
